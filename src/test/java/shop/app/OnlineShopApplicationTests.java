1cf0052ec482820fbef3d8b0baf3bd28e5fd4ad1
AhmadMdi <admimohammadi@gmail.com>
2023-11-19 14:32:42 +0330

    install springboot

---
+++ src/test/java/shop/app/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
