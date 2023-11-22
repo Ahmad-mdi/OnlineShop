c0a6dab477d82d992e1e2120f06bcf141b343957
AhmadMdi <admimohammadi@gmail.com>
2023-11-23 01:24:42 +0330

    create first ui controllers
    home and login controller for ui app

---
+++ src/main/java/shop/app/controllers/web/HomeController.java
@@ -0,0 +1,14 @@
+package shop.app.controllers.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String index(){
+        return "home";
+    }
+}
---
+++ src/main/java/shop/app/controllers/web/LoginController.java
@@ -0,0 +1,15 @@
+package shop.app.controllers.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+    @GetMapping("/login")
+    public String index(){
+        return "login";
+    }
+
+}
