f2e1350f2c1f684bcfa5759d9303ff4e6ea2a2d4
AhmadMdi <admimohammadi@gmail.com>
2023-12-02 15:19:57 +0330

    changin UserVM (DTO)
    auto complate geeter and seeter and Cunstructor method by annotations...

--- src/main/java/shop/app/helper/ui_models/UserVM.java
+++ src/main/java/shop/app/helper/ui_models/UserVM.java
@@ -1,23 +1,28 @@
 package shop.app.helper.ui_models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import shop.app.enums.UserRole;
 import shop.app.models.users.User;
 
+@Data // for geeter and setter
+@AllArgsConstructor // auto generate constructor
+@NoArgsConstructor // if == not consturctor
 public class UserVM {
     private long id;
     private String firstname;
     private String lastname;
     private String username;
     private String password;
+    @JsonIgnore// not show data in response
     private String newPassword;
     private String email;
     private UserRole role;
     private boolean enable;
     private String token;
 
-
-    public UserVM() {
-    }
     public UserVM(User user) {
         setId(user.getId());
         setEmail(user.getEmail());
@@ -29,83 +34,4 @@ public class UserVM {
         setUsername(user.getUsername());
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
-
-    public boolean isEnable() {
-        return enable;
-    }
-
-    public void setEnable(boolean enable) {
-        this.enable = enable;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
 }
