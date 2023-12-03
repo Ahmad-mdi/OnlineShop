4211e019f2e243507a0e915c74071a6cbc4b78e9
AhmadMdi <admimohammadi@gmail.com>
2023-12-03 15:59:06 +0330

    ConstasMessage
    set new errorText

--- src/main/java/shop/app/ConstantsMessage.java
+++ src/main/java/shop/app/ConstantsMessage.java
@@ -3,4 +3,5 @@ package shop.app;
 public class ConstantsMessage {
     public static String MSG="data with id %s not found";
     public static String MSG2="Incorrect username or password";
+    public static String MSG3="token is empty!";
 }
