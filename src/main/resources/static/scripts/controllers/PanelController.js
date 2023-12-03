9c6a9a750b8b566c21de35a2436bad487b08050a
AhmadMdi <admimohammadi@gmail.com>
2023-12-03 16:00:37 +0330

    change panel controller js

--- src/main/resources/static/scripts/controllers/PanelController.js
+++ src/main/resources/static/scripts/controllers/PanelController.js
@@ -1,9 +1,57 @@
-app.controller("panelCtlr", function ($scope, apiHandler, $cookies) {
+app.controller("panelCtlr", function ($scope, apiHandler, $cookies, $rootScope) {
+
+    $scope.template = "views/dashboard.html";//dashboard
+    $scope.templateName = "dashboard";
+    $scope.templateGroup = "dashboard";
+
     $scope.checkAccess = () => {
         let token = $cookies.get("userToken");
-        if (token === undefined || token == null || token === ""){
+        debugger;
+        if (token === undefined || token == null || token === "") {
             location.href = "/login";
+            return;
+        }
+        $scope.getUserInfo();
+    }
+    $scope.getUserInfo = () => {
+        apiHandler.callGet('user/getUserInfo', (response) => {
+            debugger;
+            $rootScope.userInfo = response.dataList[0];
+            $scope.user = $rootScope.userInfo;
+            console.log($scope.user);
+        }, (error) => {
+
+        }, true);
+    }
+//activation and selected menu:
+    $scope.changeMenu = (templateName) => {
+        $scope.templateName = templateName;
+        $scope.template = $scope.getMenuPrefix(templateName);
+        $scope.templateGroup = $scope.getMenuGroup(templateName);
+    }
+//for show page.html by angularJs:
+    $scope.getMenuPrefix = (templateName) => {
+        if (templateName === 'dashboard') {
+            return 'views/' + templateName + '.html';
+        } else if (templateName === 'nav-list') {
+            return 'views/site/nav/' + templateName + '.html';
+        } else {
+            return 'views/dashboard.html';
         }
     }
+// for active item by select user:
+    $scope.getMenuGroup = (templateName) => {
+        if (templateName === 'dashboard') {
+            return 'dashboard';
+        } else if (templateName === 'nav-list'
+            || templateName === 'nav-edit'
+            || templateName === 'nav-insert') {
+            return 'nav';
+        } else {
+            return 'dashboard';
+        }
+    }
+
     $scope.checkAccess();
 });
+
