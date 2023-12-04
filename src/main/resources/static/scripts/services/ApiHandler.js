844f97a5da74a1026bf3b31f09e8787952b5aeb0
AhmadMdi <admimohammadi@gmail.com>
2023-12-04 22:43:48 +0330

    change ApiHandler

--- src/main/resources/static/scripts/services/ApiHandler.js
+++ src/main/resources/static/scripts/services/ApiHandler.js
@@ -1,151 +1,38 @@
-app.service("apiHandler",function ($http,$cookies){
-    this.callPost=(url,data,onSuccess,onError,setToken)=>{
-        url = "/api/"+url;
-        let request = {
-            url: url,
-            method: "POST",
-            data: data
-        };
-        this.checkAndSetToken(request,setToken);
-        $http(request).then((response)=>{
-            if (response != null && response.data !=null){
-                let result = response.data;
-                if (result.status == "SUCCESS"){
-                    onSuccess(result);
-                }else if(result.hasError){
-                    alert(result.message);
-                }else {
-                    alert("unknown error!");
-                }
-            }
-        },(err)=>{
-            alert("exception");
-            onError(err);
-        });
-    }
-
-    this.callGet=(url,onSuccess,onError,setToken)=>{
-        url = "/api/"+url;
-        let request = {
-            url: url,
-            method: 'GET'
-        };
-        this.checkAndSetToken(request,setToken);
-        $http(request).then((response)=>{
-            if (response != null && response.data !=null){
-                let result = response.data;
-                if (result.status == "SUCCESS"){
-                    onSuccess(result);
-                }else if(result.hasError){
-                    alert(result.message);
-                }else {
-                    alert("unknown error!")
-                }
-            }
-        },(err)=>{
-            alert("exception");
-            onError(err);
-        });
-    }
-
-
-
-    this.callPut=(url,data,onSuccess,onError,setToken)=>{
-        url = "/api/"+url;
-        let request = {
-            url: url,
-            method: "PUT",
-            data:data
-        };
-        this.checkAndSetToken(request,setToken);
-        $http(request).then((response)=>{
-            if (response != null && response.data !=null){
-                let result = response.data;
-                if (result.status == "SUCCESS"){
-                    onSuccess(result);
-                }else if(result.hasError){
-                    alert(result.message);
-                }else {
-                    alert("unknown error!")
-                }
-            }
-        },(err)=>{
-            alert("exception");
-            onError(err);
-        });
-    }
-
-    this.callDelete=(url,onSuccess,onError,setToken)=>{
-        url = "/api/"+url;
-        let request = {
-            url: url,
-            method: "DELETE"
-        };
-        this.checkAndSetToken(request,setToken);
-        $http(request).then((response)=>{
-            if (response != null && response.data !=null){
-                let result = response.data;
-                if (result.status == "SUCCESS"){
-                    onSuccess(result);
-                }else if(result.hasError){
-                    alert(result.message);
-                }else {
-                    alert("unknown error!")
-                }
-            }
-        },(err)=>{
-            alert("exception");
-            onError(err);
-        });
-    }
-
-    this.checkAndSetToken = (request,setToken) => {
-        if (setToken){
-            let token = $cookies.get("userToken");
-            request.headers = {
-                'Authorization': 'Bearer '+ token
-            };
-        }
-    }
-});
-
-
-/*app.service("apiHandler", function ($http, $cookies) {
+app.service("apiHandler", function ($http, $cookies) {
 
     this.callPost = (url, data, onSuccess, onError, setToken) => {
         url = "/api/" + url;
         let request = {
             url: url,
-            method: 'POST',
+            method: "POST",
             data: data
         };
         this.checkAndSetToken(request, setToken);
         $http(request).then((response) => {
             if (response != null && response.data != null) {
                 let result = response.data;
-                if (result.status == "SUCCESS") {
+                if (result.status === "SUCCESS") {
                     onSuccess(result);
                 } else if (result.hasError) {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: result.message,
-                    })
+                        icon: "error",
+                        title: "Error",
+                        text: result.message
+                    });
                 } else {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: "unknown error!!!",
-                    })
+                        icon: "error",
+                        title: "Error",
+                        text: "unknown error!"
+                    });
                 }
             }
-
         }, (err) => {
             Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: "Exception on server",
-            })
+                icon: "error",
+                title: "Error",
+                text: "Exception on server"
+            });
             onError(err);
         });
     }
@@ -160,33 +47,27 @@ app.service("apiHandler",function ($http,$cookies){
         $http(request).then((response) => {
             if (response != null && response.data != null) {
                 let result = response.data;
-                if (result.status == "SUCCESS") {
+                if (result.status === "SUCCESS") {
                     onSuccess(result);
                 } else if (result.hasError) {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: result.message,
-                    })
+                        icon: "error",
+                        title: "Error",
+                        text: result.message
+                    });
                 } else {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: "unknown error!!!",
-                    })
+                        icon: "error",
+                        title: "Error",
+                        text: "unknown error!"
+                    });
                 }
             }
-
         }, (err) => {
-            if(err.status == 417){
-                $cookies.remove("userToken");
-                location.href = "/login";
-                return;
-            }
             Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: "Exception on server",
+                icon: "error",
+                title: "Error",
+                text: "Exception on server"
             });
             onError(err);
         });
@@ -196,77 +77,75 @@ app.service("apiHandler",function ($http,$cookies){
         url = "/api/" + url;
         let request = {
             url: url,
-            method: 'PUT',
+            method: "PUT",
             data: data
         };
         this.checkAndSetToken(request, setToken);
         $http(request).then((response) => {
             if (response != null && response.data != null) {
                 let result = response.data;
-                if (result.status == "SUCCESS") {
+                if (result.status === "SUCCESS") {
                     onSuccess(result);
                 } else if (result.hasError) {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: result.message,
-                    })
+                        icon: "error",
+                        title: "Error",
+                        text: result.message
+                    });
                 } else {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: "unknown error!!!",
-                    })
+                        icon: "error",
+                        title: "Error",
+                        text: "unknown error!"
+                    });
                 }
             }
-
         }, (err) => {
             Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: "Exception on server",
-            })
+                icon: "error",
+                title: "Error",
+                text: "Exception on server"
+            });
             onError(err);
         });
     }
-
 
     this.callDelete = (url, onSuccess, onError, setToken) => {
         url = "/api/" + url;
         let request = {
             url: url,
-            method: 'DELETE'
+            method: "DELETE"
         };
         this.checkAndSetToken(request, setToken);
         $http(request).then((response) => {
             if (response != null && response.data != null) {
                 let result = response.data;
-                if (result.status == "SUCCESS") {
+                if (result.status === "SUCCESS") {
                     onSuccess(result);
                 } else if (result.hasError) {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: result.message,
-                    })
+                        icon: "error",
+                        title: "Error",
+                        text: result.message
+                    });
                 } else {
                     Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: "unknown error!!!",
-                    })
+                        icon: "error",
+                        title: "Error",
+                        text: "unknown error!"
+                    });
                 }
             }
-
         }, (err) => {
             Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: "Exception on server",
-            })
+                icon: "error",
+                title: "Error",
+                text: "Exception on server"
+            });
             onError(err);
         });
     }
+
 
     this.checkAndSetToken = (request, setToken) => {
         if (setToken) {
@@ -276,4 +155,6 @@ app.service("apiHandler",function ($http,$cookies){
             };
         }
     }
-})*/
+});
+
+
