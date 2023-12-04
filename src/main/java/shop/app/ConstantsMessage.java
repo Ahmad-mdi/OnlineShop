ddc3478bd6aac8070a8a2ce2b45f561d53257d76
AhmadMdi <admimohammadi@gmail.com>
2023-12-04 22:41:12 +0330

    constans message

--- src/main/java/shop/app/ConstantsMessage.java
+++ src/main/java/shop/app/ConstantsMessage.java
@@ -3,5 +3,6 @@ package shop.app;
 public class ConstantsMessage {
     public static String MSG="data with id %s not found";
     public static String MSG2="Incorrect username or password";
-    public static String MSG3="token is empty!";
+    public static String m_title="please enter your title";
+    public static String m_link="please enter your link";
 }
