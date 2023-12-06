e96a5df91ffb57a2af35a95de9408b51a4c77d7f
AhmadMdi <admimohammadi@gmail.com>
2023-12-06 11:16:41 +0330

    getAll-add-chngeOrder
    set

--- src/main/java/shop/app/Services/site/SliderService.java
+++ src/main/java/shop/app/Services/site/SliderService.java
@@ -1,9 +1,13 @@
 package shop.app.Services.site;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import shop.app.helper.exceptions.DataNotFoundException;
+import shop.app.models.site.Nav;
 import shop.app.models.site.Slider;
 import shop.app.repositories.site.SliderRepository;
 
@@ -33,9 +37,59 @@ public class SliderService {
 
     }
 
+    public List<Slider> getAll(Integer pageSize, Integer pageNumber) {
+        Pageable pagination = PageRequest.of(pageNumber, pageSize, Sort.by("itemOrder"));
+        Page<Slider> all = sliderRepository.findAll(pagination);
+        return all.toList();
+    }
+
+    //totalCount pagination:
+    public long getAllCount() {
+        return sliderRepository.count();
+    }
+
     //add sliders to db:
     public Slider add(Slider data) {
+        Slider lastItem = sliderRepository.findTopByOrderByItemOrderDesc();
+        if (lastItem != null && lastItem.getItemOrder() > 0)
+            data.setItemOrder(lastItem.getItemOrder() + 1);
         return sliderRepository.save(data);
+    }
+
+    public Slider changeOrder(long id, int direction) throws Exception {
+        Slider item = getById(id);
+        if (item == null)
+            throw new Exception("item not found!");
+        switch (direction) {
+            case 1:
+                //up
+                if (item.getItemOrder() <= 1)
+                    return item;
+                Slider siblingItem = sliderRepository.findTopByItemOrder(item.getItemOrder() - 1);
+                if (siblingItem == null)
+                    item.setItemOrder(item.getItemOrder() - 1);
+                else {
+                    item.setItemOrder(siblingItem.getItemOrder());
+                    siblingItem.setItemOrder(item.getItemOrder() + 1);
+                    sliderRepository.save(siblingItem);
+                }
+                break;
+            case 0:
+                //down
+                Slider siblingItem2 = sliderRepository.findTopByItemOrder(item.getItemOrder() + 1);
+                if (siblingItem2 == null) {
+                    Slider lastOrderItem = sliderRepository.findTopByOrderByItemOrderDesc();
+                    if (item.getItemOrder() < lastOrderItem.getItemOrder())
+                        item.setItemOrder(item.getItemOrder() + 1);
+                } else {
+                    item.setItemOrder(siblingItem2.getItemOrder());
+                    siblingItem2.setItemOrder(item.getItemOrder() - 1);
+                    sliderRepository.save(siblingItem2);
+                }
+                break;
+        }
+        sliderRepository.save(item);
+        return item;
     }
 
     //update slider find by id:
