73fdfd1c06e174a31bb59f3abe342869c67bfbee
AhmadMdi <admimohammadi@gmail.com>
2023-12-06 11:13:53 +0330

    findTopByOrderByItemOrderDesc() and findTopByItemOrder(int itemOrder)
    in slider repository for chngeOrderItem by users

--- src/main/java/shop/app/repositories/site/SliderRepository.java
+++ src/main/java/shop/app/repositories/site/SliderRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 @Repository
 public interface SliderRepository extends JpaRepository<Slider,Long> {
     List<Slider> findAllByEnableIsTrue(Sort sort);
+    Slider findTopByOrderByItemOrderDesc();
+    Slider findTopByItemOrder(int itemOrder);
 }
